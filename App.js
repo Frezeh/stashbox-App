83cbd3e1e9e78cd058db7c946e63c843688358c3
Frezeh <ezehfrank87@gmail.com>
2022-01-17 19:04:44 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,43 +1,13 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Slider from "./components/slider";
-import BottomTabNavigator from "./navigation/tabNavigation";
-import { NavigationContainer } from "@react-navigation/native";
-import Dashboard from "./screens/dashboard";
-import MyCards from "./screens/myCards";
-import Registration from "./screens/registration";
+import { StyleSheet, Text, View, SafeView } from "react-native";
 
 export default function App() {
-  const [toggleNavigation, setToggleNavigation] = useState(false);
-  const [toggleSlider, setToggleSlider] = useState(true);
-  const [toggleRegistration, setToggleRegistration] = useState(false);
-
   return (
-    <View style={styles.container}>
-      {toggleSlider && (
-        <Slider
-          setToggleNavigation={setToggleNavigation}
-          setToggleRegistration={setToggleRegistration}
-          setToggleSlider={setToggleSlider}
-        />
-      )}
-
-      {toggleNavigation && (
-        <NavigationContainer>
-          <BottomTabNavigator />
-        </NavigationContainer>
-      )}
-
-      {toggleRegistration && (
-        <Registration
-          setToggleNavigation={setToggleNavigation}
-          setToggleRegistration={setToggleRegistration}
-          setToggleSlider={setToggleSlider}
-        />
-      )}
+    <SafeView style={styles.container}>
+     <Text>{'Get Started'}</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeView>
   );
 }
 
